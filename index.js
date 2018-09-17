3c4c0a07adc991258a8d4120c195f9c85c28429d
matthew.serre <matthew.serre@gmail.com>
2018-09-17 00:04:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function iLoveTheBeatles (number) {
   var array = [];
   do {
     array.push("I love the Beatles!");
-    incrementVariable();
+    number++;
   } while (number < 15);
   return array;
 }
